be1859af213769e5d4592ebd70efa93efb8345c8
Nauzan <nauzan0401@gmail.com>
2025-02-13 21:06:22 -0500

    ejemplo paraiso

---
+++ script.js
@@ -0,0 +1,5 @@
+// Validación del formulario de contacto
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('¡Gracias por contactarnos! Te responderemos pronto.');
+});
